ff9487c6167f17b92278c090c32596be2d1281d3
VIJAYAPHOENIX <vijaygrandhi860@gmail.com>
2025-03-13 00:23:43 +0530

    Day-13 added and practiced

---
+++ Day-13.py
@@ -0,0 +1,5 @@
+# Debugging techniques
+# if you want to debug a program step by step can be done by using "THONEY" or "PYTHON TUTOR .COM" .
+# if you mess with a code don't overthink . just take a break
+# compile the program regularly not untill you write all the code.
+# Or you can use "stackoverflow.com" to clarify your doubts
